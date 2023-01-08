c7d079507322079292d390caa5978b367306e60d
‘gpm-github’ <gdpachecom@gmail.com>
2023-01-08 16:58:28 -0500

    volviendo al inicio

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hola, Mundo, desde PERU";
+const hello = () => "Hello World from Spain";
 console.log(hello());
 exports.hello = hello;
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola, Mundo, desde PERU");
+    expect(hello.hello()).toEqual("Hello World from Spain");
   });
 });
